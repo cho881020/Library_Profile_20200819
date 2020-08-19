02b0ee865e34bfae778059cd191ca470bfa65c62
조경진 <kj_cho@idealidea.co.kr>
2020-08-19 20:01:06 +0900

    프로필 사진을 누르면 크게 보기 화면으로 들어오기

--- app/src/main/java/kr/co/tjoeun/library_profile_20200819/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_profile_20200819/MainActivity.kt
@@ -1,7 +1,9 @@
 package kr.co.tjoeun.library_profile_20200819
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -13,6 +15,15 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        profilePhotoImg.setOnClickListener {
+
+//            사진을 크게 보는 별개의 액티비티로 이동
+
+            val myIntent = Intent(mContext, ViewProfilePhotoActivity::class.java)
+            startActivity(myIntent)
+
+        }
 
     }
 
---
+++ app/src/main/java/kr/co/tjoeun/library_profile_20200819/ViewProfilePhotoActivity.kt
@@ -0,0 +1,23 @@
+package kr.co.tjoeun.library_profile_20200819
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProfilePhotoActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile_photo)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
