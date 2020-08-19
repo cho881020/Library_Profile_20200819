6ee2df2843a3089f9b6ea7324c7334a4b62b63fe
조경진 <kj_cho@idealidea.co.kr>
2020-08-19 20:54:55 +0900

    Glide 라이브러리로 웹 이미지 출력  - Manifest에서 인터넷 권한 획득

--- app/src/main/java/kr/co/tjoeun/library_profile_20200819/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_profile_20200819/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.tjoeun.library_profile_20200819
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -28,6 +29,9 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        Glide로 웹상의 차은우 사진을 프사로 반영
+        Glide.with(mContext).load("https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/170924_%ED%91%B8%EB%93%9C%ED%8A%B8%EB%9F%AD%EC%9D%B4%EB%B2%A4%ED%8A%B8.jpg/250px-170924_%ED%91%B8%EB%93%9C%ED%8A%B8%EB%9F%AD%EC%9D%B4%EB%B2%A4%ED%8A%B8.jpg").into(profilePhotoImg)
 
     }
 
